888b9d1d16e4d1ddc8df7dcab2f458502ad63e92
thiagoboneth <thiago.luis@mercadolivre.com>
2023-02-24 02:22:05 -0300

    input data and flux analitc

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use std::io;
+
+fn convert_to_int(data_input: & String) -> i32{
+    let x = data_input.trim().parse::<i32>().unwrap();
+    x
+}
+
 fn main() {
-    let number1 = 24;
-    let number2 = 42;
-    if number1 > number2 {
-        print!("{} > {}", number1, number2)
+    let mut number1 = String::new();
+    io::stdin().read_line(&mut number1).expect("Erro ao ler number1");
+
+    let mut number2 = String::new();
+    io::stdin().read_line(&mut number2).expect("Erro ao ler number2");
+
+    if convert_to_int(&number1) > convert_to_int(&number2){
+        print!("O numero {} é maior que {}", number1, number2);
     }
     else {
-        print!("{} <= {}", number1, number2)
+        print!("O numero {} é menor ou igual que {}", number1, number2)
     }
 }
